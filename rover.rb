3e595ba45dff804c1e6409ffe41ddebd950c2803
Sandy Lai <sandywslai@gmail.com>
2015-04-04 12:07:14 -0400

    Created spoken greeting and new method to allow for user to create multiple plateaux.

--- rover.rb
+++ rover.rb
@@ -23,22 +23,21 @@ class Rover
     end
   end
 
-  #Rover updates position to confirmed position from requester.
+  #Rover moves to verified position
   def move(requester)
     @x = requester.check_x
     @y = requester.check_y
   end
 
-  #track_heading tracks changes with direction as assignment by increment/decrement from array of values.  Allows us to keep track of heading as we check if instructions are valid.
   def turn(direction)
-    compass_points = ['N', 'E', 'S', 'W']
-
-    if direction == 'R' && @heading == 'W'
-      @heading = 'N'
-    elsif direction == 'R'
-      @heading = compass_points[(compass_points.index(@heading) + 1)]
+    if (direction == "R" && @heading == "W") || (direction =="L" && @heading == "E")
+      @heading = "N"
+    elsif (direction == "R" && @heading == "E") || (direction =="L" && @heading == "W")
+      @heading = "S"
+    elsif (direction == "R" && @heading == "N") || (direction =="L" && @heading == "S")
+      @heading = "E"
     else
-      @heading = compass_points[(compass_points.index(@heading) - 1)]
+      @heading = "W"
     end
   end
 end
@@ -46,52 +45,62 @@ end
 #MissionControl is responsible for assigning and ordering rovers around
 class MissionControl
   attr_reader :check_x, :check_y, :check_heading, :rover_list
-  def initialize(plateau)
-    @plateau = plateau
-    puts "Welcome to Mission Control.  How many rovers would you like to deploy?"
-    gets.chomp.to_i.times{run_rover_commands}
+  def initialize
+    puts "Bitmaker SpaceLabs Rover Deployment System.  Enter your city."
+    `say "This is the Bitmaker SpaceLabs Rover Deployment System.  Which city are you in?"`
+    greeting = "Welcome to #{gets.chomp.capitalize.strip} Mission Control."
+    puts greeting + " Enter the names of the plateaux you wish to deploy rovers to, separated by commas:"
+    `say "#{greeting}"`
+    create_plateaux
+  end
+
+  def create_plateaux
+    plateau_list = gets.chomp.upcase.split(",")
+    plateau_list.each do |plateau_name|
+      @plateau = Plateau.new(plateau_name.strip.capitalize)
+      puts "How many rovers would you like to deploy?"
+      gets.chomp.to_i.times{run_rover_commands}
+    end
   end
 
   def get_instructions(rover)
+    @check_x, @check_y = rover.x, rover.y
     puts "Where would you like the rover to move? Enter L for left, R for right and M for forward in the current heading i.e. LMMMMRMMMRMMMLM"
     read_instructions(rover, gets.chomp.upcase.split(""))
   end
 
   def read_instructions(rover, set_of_instructions)
-    print "These instructions were executed: "
     set_of_instructions.each do |instruction|
       if instruction == 'M'
-        next_move(rover)
-        rover.move(self) unless stop_rover?(rover)
+          next_move(rover)
+          break unless confirm_move(rover)
       else
         rover.turn(instruction)
       end
-      print instruction
     end
     @plateau.rover_list.push(rover)
   end
 
-  def stop_rover?(rover)
-    if @plateau.fall?(@check_x, @check_y)
-      puts "\nThe next instruction M would make the rover fall off the plateau. The rover has stopped safely at #{rover.x}, #{rover.y} heading #{rover.heading}. Please provide new instructions."
+  def confirm_move(rover)
+    if @plateau.fall?(@check_x, @check_y) || @plateau.collide?(@check_x, @check_y)
+      puts "Execution aborted.  The rover has stopped safely at #{rover.x}, #{rover.y} heading #{rover.heading}. Please provide new instructions."
       get_instructions(rover)
-    elsif @plateau.collide?(@check_x, @check_y)
-      puts "\nThe next instruction M would make the rover crash into another rover. The rover has stopped safely at #{rover.x}, #{rover.y} heading #{rover.heading}. Please provide new instructions."
-      get_instructions(rover)
+      return false
+    else
+      rover.move(self)
     end
   end
 
   #next_move tracks next location as assignment by increment/decrement based on heading
   def next_move(rover)
-    @check_x, @check_y = rover.x, rover.y
     case rover.heading
-      when 'N'
+      when "N"
         @check_y += 1
-      when 'S'
+      when "S"
         @check_y -= 1
-      when 'E'
+      when "E"
         @check_x += 1
-      when 'W'
+      when "W"
         @check_x -= 1
     end
   end
@@ -104,9 +113,9 @@ class MissionControl
 end
 
 class Plateau
-  attr_accessor :rover_list
-  def initialize
-    puts "How big is the plateau?  ___ by ___? (Please enter the numbers like this: 5 5)."
+  attr_accessor :rover_list#, :plateau_name
+  def initialize(plateau_name)
+    puts "How big is #{plateau_name}?  ___ by ___? (Please enter the numbers like this: 5 5)."
     size = gets.chomp.split
     @max_x = size[0].to_i
     @max_y = size[1].to_i
@@ -114,19 +123,22 @@ class Plateau
   end
 
   def fall?(x, y)
-    x > @max_x || y > @max_y
+    if x < 0 || x > @max_x || y < 0 ||y > @max_y
+      puts "The next instruction would make the rover fall off the plateau."
+      return true
+    end
+    false
   end
 
   def collide?(x, y)
     @rover_list.each do |listed_rover|
       if x == listed_rover.x && y == listed_rover.y
+        puts "The next instruction would make the rover crash into another rover."
         return true
-        break
       end
     end
     false
   end
 end
 
-tharsis = Plateau.new
-nasa = MissionControl.new(tharsis)
+MissionControl.new
