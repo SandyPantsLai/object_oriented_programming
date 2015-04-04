54570a1481e228c259a755198b5a3ee9df0d6a56
Sandy Lai <sandywslai@gmail.com>
2015-04-03 20:46:40 -0400

    Added move method for Rover to update location based on heading.

--- rover.rb
+++ rover.rb
@@ -5,6 +5,20 @@ class Rover
     @y = y
     @heading = heading
   end
+
+  #The SW corner of the grid is 0,0
+  def move
+    case rover_heading
+      when @heading == 'N'
+        @y += 1
+      when @heading == 'S'
+        @y -= 1
+      when @heading == 'E'
+        @x += 1
+      when @heading == 'W'
+        @x -= 1
+      end
+  end
 end
 
 #Accept instruction and decide whether to tell rover to turn or move
