aed7c4e8c794bb353996b8eb232e12e08c310ec7
Sandy Lai <sandywslai@gmail.com>
2015-04-03 21:17:23 -0400

    Fixed case statements and called read_instruction.

--- rover.rb
+++ rover.rb
@@ -1,21 +1,21 @@
 class Rover
 
   def initialize(position) #Array created from split of user input
-    @x = position[0]
-    @y = position[1]
+    @x = position[0].to_i
+    @y = position[1].to_i
     @heading = position[2]
   end
 
   #The SW corner of the grid is 0,0
   def move
-    case rover_heading
-      when @heading == 'N'
+    case @heading
+      when 'N'
         @y += 1
-      when @heading == 'S'
+      when 'S'
         @y -= 1
-      when @heading == 'E'
+      when 'E'
         @x += 1
-      when @heading == 'W'
+      when 'W'
         @x -= 1
       end
   end
@@ -34,11 +34,13 @@ class Rover
 end
 
 #Accept instruction and decide whether to tell rover to turn or move
-def read_instruction(instruction)
-  if instruction == 'M'
-    rover.move
-  else
-    rover.turn(direction)
+def read_instruction(rover, set_of_instructions)
+  set_of_instructions.each do |instruction|
+    if instruction == 'M'
+      rover.move
+    else
+      rover.turn(instruction)
+    end
   end
 end
 
@@ -51,3 +53,7 @@ gets.chomp.split
 #Get starting position to create rover instance
 puts "What is Rover 1's position?  (0,0) is the lower left (SW corner) of the plateau.  Please enter as its x and y coordinates and heading with spaces i.e. 5 5 N"
 rover1 = Rover.new(gets.chomp.upcase.split)
+
+#Get set of instructions from user
+puts "Where would you like the rover to move? Enter L for left, R for right and M for forward in the current heading i.e. LMMMMRMMMRMMMLM"
+read_instruction(rover1, gets.chomp.upcase.split(""))
