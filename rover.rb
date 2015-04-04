e1bf8ee5d20bfa070c07ecd716265beb86382c2b
Sandy Lai <sandywslai@gmail.com>
2015-04-03 20:54:21 -0400

    Added turn method to change rover's heading.

--- rover.rb
+++ rover.rb
@@ -19,6 +19,17 @@ class Rover
         @x -= 1
       end
   end
+
+  #Heading changes with direction as assignment by increment/decrement from array of values
+  def turn(direction)
+    compass_points = ['N', 'E', 'S', 'W']
+    if direction == 'R' && @heading == 'W'
+      @heading = 'N'
+    elsif direction == 'R'
+      @heading = compass_points[compass_points.index(@heading) + 1]
+    else
+      @heading = compass_points[compass_points.index(@heading) - 1]
+  end
 end
 
 #Accept instruction and decide whether to tell rover to turn or move
